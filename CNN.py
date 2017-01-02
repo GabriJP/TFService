cec6f005a053e29c55595bb029176c761ed5ab44
Gabriel <gabriel.jimenez102@alu.ulpgc.es>
2017-01-02 19:28:07 +0000

    Code reformat

---
+++ CNN.py
@@ -0,0 +1,166 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+
+from os.path import join
+
+
+def maxpool2d(x, k=2):
+    # MaxPool2D wrapper
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                          padding='SAME')
+
+
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+def conv_net(x, weights, biases, frame_dimensions, layer_dropout=None):
+    # Reshape input picture
+    # x = tf.reshape(x, shape=[-1, 172, 380, 1])
+    x = tf.reshape(x, shape=[-1, frame_dimensions[0], frame_dimensions[1], 1])
+
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    # Apply Dropout
+    if layer_dropout is not None:
+        fc1 = tf.nn.dropout(fc1, layer_dropout)
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return tf.nn.softmax(out) if layer_dropout is None else out
+
+
+def create_cnn(data_set, save_path, training_iters, learning_rate=0.001, batch_size=32, display_step=10, dropout=0.75):
+    # Input layer
+    x = tf.placeholder(tf.float32, [None, data_set.frame_pixels()])
+    # Output layer
+    y = tf.placeholder(tf.float32, [None, data_set.get_number_of_classes()])
+    # Dropout Tensor
+    keep_prob = tf.placeholder(tf.float32)
+
+    # Store layers weight & bias
+    weights, biases = get_weights_and_biases(data_set.get_number_of_classes())
+
+    # Construct model
+    pred = conv_net(x, weights, biases, data_set.get_frame_dimensions(), keep_prob)
+
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # Evaluate model
+    correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
+        step = 1
+        # Keep training until reach max iterations
+        while step * batch_size < training_iters:
+            batch_labels, batch_frames = data_set.next_training_batch(batch_size)
+            # batch_frames = list(map((lambda frame: frame.flatten()), batch_frames))
+            batch_frames = [array.flatten() for array in batch_frames]
+            # Run optimization op (backprop)
+            sess.run(optimizer, feed_dict={x: batch_frames, y: batch_labels, keep_prob: dropout})
+            if step % display_step == 0:
+                # Calculate batch loss and accuracy
+                loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_frames, y: batch_labels, keep_prob: 1.})
+                print("Iter " + str(step * batch_size) + ", Minibatch Loss= " +
+                      "{:.6f}".format(loss) + ", Training Accuracy= " +
+                      "{:.5f}".format(acc))
+
+            step += 1
+        print("Optimization Finished!")
+
+        for i in range(0, 244, batch_size):
+            batch_labels, batch_frames = data_set.next_test_batch(batch_size)
+            batch_frames = list(map((lambda frame: frame.flatten()), batch_frames))
+            print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: batch_frames, y: batch_labels, keep_prob: 1.}))
+
+        save_path = tf.train.Saver().save(sess, join(save_path, "model"))
+        print("Model saved in file: %s" % save_path)
+        sess.close()
+
+    tf.reset_default_graph()
+
+
+def play_cnn(meta_dataset, output, video):
+    n_input = meta_dataset['frame_pixels']
+    n_classes = meta_dataset['n_classes']
+
+    x = tf.placeholder(tf.float32, [None, n_input])
+    # Store layers weight & bias
+    weights, biases = get_weights_and_biases(n_classes)
+
+    # Add ops to save and restore all the variables.
+    saver = tf.train.Saver()
+
+    # Construct model
+    pred = conv_net(x, weights, biases, meta_dataset['shape'])
+
+    # ----------------------------- Video capture
+
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(video)
+
+    # Launch the graph
+    with tf.Session() as sess:
+        saver.restore(sess, join(output, 'model'))
+
+        while True:
+            ret, img = cap.read()
+            if img is None:
+                break
+            resized = cv2.resize(img, meta_dataset['shape'], interpolation=cv2.INTER_AREA)
+            gray = np.asarray(cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY))
+
+            cv2.imshow('Capture', img)
+            frame = gray.reshape(-1, meta_dataset['shape'][0] * meta_dataset['shape'][1])
+            res = sess.run(pred, feed_dict={x: frame})
+            print(meta_dataset['labels'].get(tuple(res[0])))
+
+            ch = 0xFF & cv2.waitKey(1)
+            if ch == 27:
+                break
+
+    cv2.destroyAllWindows()
+    tf.reset_default_graph()
+
+
+def get_weights_and_biases(n_classes):
+    return {
+               # 5x5 conv, 1 input, 32 outputs
+               'wc1': tf.Variable(tf.random_normal([5, 5, 1, 64])),
+               # 5x5 conv, 32 inputs, 64 outputs
+               'wc2': tf.Variable(tf.random_normal([5, 5, 64, 128])),
+               # fully connected, 7*7*64 inputs, 1024 outputs
+               'wd1': tf.Variable(tf.random_normal([35 * 20 * 128, 256])),
+               # 1024 inputs, 10 outputs (class prediction)
+               'out': tf.Variable(tf.random_normal([256, n_classes]))
+           }, {
+               'bc1': tf.Variable(tf.random_normal([64])),
+               'bc2': tf.Variable(tf.random_normal([128])),
+               'bd1': tf.Variable(tf.random_normal([256])),
+               'out': tf.Variable(tf.random_normal([n_classes]))
+           }
--- Main.py
+++ Main.py
@@ -3,23 +3,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from sys import stderr
 from DataSet import DataSet
-from CNNCreator import create_cnn
-from CNNPlayer import play_cnn
+from CNN import create_cnn, play_cnn
 
 import argparse
-import tensorflow as tf
 
 """
-Program usage:
-python ProgramName.py classes=directory resize='width'x'height' crop=x_from:y_from:x_to:y_to
-train=percentage_for_training val=percentage_for_validation test=percentage_for_validation out=output_file
-
 Example:
 -r 0.75 -e 0.25 -i 3000 Other/Classes/Carreteras 140x80 0:0:140:80 Other/Output/
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("directory", help="Directory where to find the directories of classes with videos.")
 parser.add_argument("resize", help="Resize frames to this. 140x80.")
 parser.add_argument("crop", help="Crop resized frames to this. 0:0:140:80 (Left:Up:Right:Bottom).")
 parser.add_argument("output", help="Directory where to save the dataset and the network data.")
@@ -29,9 +22,11 @@ parser.add_argument("-e", "--test_p", type=float, help="Percentage of frames to 
                     default=0.2)
 parser.add_argument("-i", "--iterations", type=int,
                     help="Number of frames presented to the net in the training process. 3000.", default=3000)
+parser.add_argument("-c", "--create", help="Directory where to find the directories of classes with videos.")
+parser.add_argument("-p", "--play", help="Use this video to play the loaded CNN")
 args = parser.parse_args()
 
-classes_root = args.directory
+classes_root = args.create
 resize = tuple(map(int, args.resize.split("x")))
 crop = tuple(map(int, args.crop.split(":")))
 output = args.output
@@ -49,10 +44,11 @@ if not 0 <= test + train <= 1:
 validation = 1 - train - test
 iterations = args.iterations
 
-# data_set = DataSet.from_directory(classes_root, resize, crop, train, test)
-# data_set.to_file(output)
-data_set = DataSet.from_file(output)
+if classes_root:
+    data_set = DataSet.from_directory(classes_root, resize, crop, train, test)
+    data_set.to_file(output)
+    create_cnn(data_set, output, iterations)
 
-# create_cnn(data_set, output, iterations)
-# tf.reset_default_graph()
-play_cnn(data_set.get_metadata(), output)
+if args.play:
+    data_set = DataSet.from_file(output)
+    play_cnn(data_set.get_metadata(), output, args.play)
